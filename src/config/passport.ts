794d71f896997f9d2db1f30f31cbaadcbe3a1ebd
masb0ymas <fajriajjha@gmail.com>
2020-08-15 23:03:41 +0700

    change passport ts

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,19 +1,20 @@
-import 'dotenv/config'
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
+import models from 'models'
 
-// load up the user model
-const User = require('#modules/User/user.model')
+const { User } = models
 
-module.exports = function(passport) {
+require('dotenv').config()
+
+module.exports = function (passport: any) {
   const jwtOpts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
     secretOrKey: process.env.JWT_SECRET, // pass jwt
   }
   passport.use(
     'jwt',
-    new JwtStrategy(jwtOpts, async function(jwtPayload, done) {
+    new JwtStrategy(jwtOpts, async function (jwtPayload, done) {
       try {
-        const user = await User.findById(jwtPayload._id)
+        const user = await User.findByPk(jwtPayload.id)
         if (!user) {
           return done(null, false)
         }
