3b02b64ea4ae36596c9a6030915e0d02d862b555
masb0ymas <fajriajjha@gmail.com>
2020-08-15 23:07:28 +0700

    remove old routes

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import PublicRoute from './public'
-import AdminRoute from './admin'
-
-const router = express.Router()
-router.use('/v1', PublicRoute)
-router.use('/v1', AdminRoute)
-
-module.exports = router
