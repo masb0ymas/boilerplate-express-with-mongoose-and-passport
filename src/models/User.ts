2bab87b87d772217e66aa42a0331610a0965cf5c
masb0ymas <fajriajjha@gmail.com>
2020-08-16 13:40:49 +0700

    fix: model user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,16 @@
 /* eslint-disable no-unused-vars */
 import { Schema, model, Document } from 'mongoose'
 
-interface UserAttributes {
-  nama: string
+export interface UserAttributes {
+  fullName: string
+  email: string
+  password?: string | null
+  phone: string
+  active?: boolean | null
+  tokenVerify?: string | null
+  newPassword?: string
+  confirmNewPassword?: string
+  Role: string
   createdAt?: Date
   updatedAt?: Date
 }
@@ -11,7 +19,13 @@ interface UserCreationAttributes extends UserAttributes, Document {}
 
 const UserSchema = new Schema(
   {
-    nama: { type: String, required: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    active: { type: Boolean, default: false, required: true },
+    tokenVerify: { type: String, required: true },
+    Role: { type: Schema.Types.ObjectId, required: true, ref: 'Roles' },
   },
   { timestamps: true }
 )
