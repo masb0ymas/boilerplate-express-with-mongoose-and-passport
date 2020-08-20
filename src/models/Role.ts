60c63d8c75bf79c82adc89d197d1e8ae838bc1ee
masb0ymas <fajriajjha@gmail.com>
2020-08-20 12:08:04 +0700

    add: export interface role

--- src/models/Role.ts
+++ src/models/Role.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Schema, model, Document } from 'mongoose'
 
-interface RoleAttributes {
+export interface RoleAttributes {
   nama: string
   createdAt?: Date
   updatedAt?: Date
