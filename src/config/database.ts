f4958d7b93606cb4f0e862b967464390f6a07b33
masb0ymas <fajriajjha@gmail.com>
2020-08-30 23:01:42 +0700

    fix: change database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,22 +8,21 @@ const HOST = process.env.MONGODB_HOST
 const PORT = process.env.MONGODB_PORT
 const COLLECTION = process.env.MONGODB_DATABASE
 
-const dbConfig = {
+const setUri = `mongodb://${HOST}:${PORT}/${COLLECTION}`
+const setOptions = {
   user: USERNAME,
   pass: PASSWORD,
-  url: `mongodb://${HOST}:${PORT}/${COLLECTION}`,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 }
 
 const initialMongoDB = () => {
   // Connecting to the database
   mongoose.Promise = global.Promise
   mongoose
-    .connect(dbConfig.url, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
+    .connect(setUri, setOptions)
     .then(() => {
       console.log('Successfully connected to the MongoDB database')
     })
