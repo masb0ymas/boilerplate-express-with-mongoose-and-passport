db4ba2108b63f2907ce7311e858ab585de815d18
masb0ymas <fajriajjha@gmail.com>
2020-09-27 20:41:53 +0700

    fix: auth controller

--- src/controllers/Auth/controller.ts
+++ src/controllers/Auth/controller.ts
@@ -2,8 +2,9 @@
 import { Request, Response } from 'express'
 import routes from 'routes/public'
 import asyncHandler from 'helpers/asyncHandler'
-import { verifyToken } from 'helpers/Common'
+import { verifyToken } from 'helpers/Token'
 import { TokenAttributes } from 'models/User'
+import Authorization from 'middlewares/Authorization'
 import AuthService from './service'
 
 routes.post(
@@ -20,21 +21,19 @@ routes.post(
   '/auth/sign-in',
   asyncHandler(async function signIn(req: Request, res: Response) {
     const formData = req.getBody()
-    const { token, expiresIn, tokenType, uid } = await AuthService.signIn(
-      formData
-    )
+    const { token, expiresIn, tokenType } = await AuthService.signIn(formData)
 
     return res.status(200).json({
       token,
       expiresIn,
       tokenType,
-      uid,
     })
   })
 )
 
 routes.get(
   '/profile',
+  Authorization,
   asyncHandler(async function getProfile(req: Request, res: Response) {
     // @ts-ignore
     const token: TokenAttributes = verifyToken(req.getHeaders())
