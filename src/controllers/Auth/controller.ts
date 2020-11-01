f4f72b30e806cbc8897fa557f496c98cb62d22c2
masb0ymas <fajriajjha@gmail.com>
2020-11-01 11:06:11 +0700

    fix: controller auth

--- src/controllers/Auth/controller.ts
+++ src/controllers/Auth/controller.ts
@@ -2,9 +2,9 @@
 import { Request, Response } from 'express'
 import routes from 'routes/public'
 import asyncHandler from 'helpers/asyncHandler'
-import { verifyToken } from 'helpers/Token'
-import { TokenAttributes } from 'models/User'
+import { currentToken, verifyToken } from 'helpers/Token'
 import Authorization from 'middlewares/Authorization'
+import ResponseSuccess from 'modules/Response/ResponseSuccess'
 import AuthService from './service'
 
 routes.post(
@@ -12,8 +12,9 @@ routes.post(
   asyncHandler(async function signUp(req: Request, res: Response) {
     const formData = req.getBody()
     const { data, message } = await AuthService.signUp(formData)
+    const buildResponse = ResponseSuccess.get(message)
 
-    return res.status(201).json({ data, message })
+    return res.status(201).json({ ...buildResponse, data })
   })
 )
 
@@ -23,11 +24,14 @@ routes.post(
     const formData = req.getBody()
     const { token, expiresIn, tokenType } = await AuthService.signIn(formData)
 
-    return res.status(200).json({
-      token,
-      expiresIn,
-      tokenType,
-    })
+    return res
+      .cookie('token', token, {
+        maxAge: Number(expiresIn) * 1000, // 7 Days
+        httpOnly: true,
+        path: '/v1',
+        secure: process.env.NODE_ENV === 'production',
+      })
+      .json({ token, expiresIn, tokenType })
   })
 )
 
@@ -35,10 +39,13 @@ routes.get(
   '/profile',
   Authorization,
   asyncHandler(async function getProfile(req: Request, res: Response) {
-    // @ts-ignore
-    const token: TokenAttributes = verifyToken(req.getHeaders())
-    const data = await AuthService.profile(token)
+    const getToken = currentToken(req)
+    const token = verifyToken(getToken)
 
-    return res.status(200).json({ data })
+    // @ts-ignore
+    const data = await AuthService.profile(token)
+    const buildResponse = ResponseSuccess.get()
+
+    return res.status(200).json({ ...buildResponse, data })
   })
 )
