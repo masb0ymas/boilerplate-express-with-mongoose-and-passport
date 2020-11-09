711da976b456bf5d1b86ec4dbaec34d38ccf6e28
masb0ymas <fajriajjha@gmail.com>
2020-11-09 14:56:38 +0700

    fix: return routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,36 +1,30 @@
 // eslint-disable-next-line no-unused-vars
 import express, { Request, Response, NextFunction } from 'express'
+import BuildResponse from 'modules/Response/BuildResponse'
+import ResponseError from 'modules/Response/ResponseError'
 import publicRoute from './public'
 
 const router = express.Router()
 
 /* Home Page. */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
-  res.render('index', {
-    title: 'Express TS',
-    description: 'Powered by Nusantech',
+  const buildResponse = BuildResponse.get({
+    message: 'Express TS, Powered by Nusantech',
   })
+  return res.json(buildResponse)
 })
 
 /* Forbidden Page. */
 router.get('/v1', function (req: Request, res: Response, next: NextFunction) {
-  res.render('index', {
-    title: 'Hayo Mau ngapain ??',
-    description: 'Forbidden Access',
-    code: '403',
-  })
+  throw new ResponseError.Forbidden('forbidden, wrong access endpoint')
 })
 
 /* Declare Route */
 router.use('/v1', publicRoute)
 
 /* Not Found Page. */
-router.get('*', function (req: Request, res: Response, next: NextFunction) {
-  res.render('index', {
-    title: 'Oops, Halaman tidak ditemukan!',
-    description: 'Not Found',
-    code: '404',
-  })
-})
+// router.get('*', function (req: Request, res: Response, next: NextFunction) {
+//   throw new ResponseError.NotFound('endpoint not found')
+// })
 
 export default router
