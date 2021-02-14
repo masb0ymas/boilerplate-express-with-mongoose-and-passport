485a46fbe1fc82877b2a190282eaa8cd69da7600
masb0ymas <fajriajjha@gmail.com>
2021-02-14 21:08:33 +0700

    fix: role test

--- src/controllers/Role/__tests__/role.test.ts
+++ src/controllers/Role/__tests__/role.test.ts
@@ -3,6 +3,8 @@ import request from 'supertest'
 // @ts-ignore
 import app from '../../../app'
 
+jest.useFakeTimers()
+
 describe('Get Role Routes', () => {
   it('should be get all role data', (done) => {
     request(app)
