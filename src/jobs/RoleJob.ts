6141a7f5da1e5e52bc9efd073d1fc3985f6ecb90
masb0ymas <fajriajjha@gmail.com>
2021-04-08 14:30:23 +0700

    fix: job role

--- src/jobs/RoleJob.ts
+++ src/jobs/RoleJob.ts
@@ -1,7 +1,4 @@
 import cron from 'node-cron'
-import models from 'models'
-
-const { Role } = models
 
 class RoleJob {
   /**
@@ -10,8 +7,7 @@ class RoleJob {
   public static getRole() {
     // Run this job every 30 minutes
     const task = cron.schedule('30 * * * *', async () => {
-      const data = await Role.find()
-      console.log('Running task check get role by id', { data })
+      console.log('Running task every 30 minutes')
     })
 
     return task
